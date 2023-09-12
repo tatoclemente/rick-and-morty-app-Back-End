5ebd9c16331c7ebc4e5221b9a63b62cf66ea2c50
tatoclemente <tatoapuros@gmail.com>
2023-09-11 21:28:18 -0300

    New Deploy Server in Render

--- src/index.js
+++ src/index.js
@@ -5,11 +5,6 @@ require('dotenv').config();
 
 const port = process.env.PORT || 3001;
 
-// server.listen(PORT, async () =>{
-//   console.log("Server raised in port: " + PORT)
-//   await conn.sync({ alter: true })
-//   })
-
 conn.sync({ alter: true })
 .then(() => {
   server.listen(port, () =>{
